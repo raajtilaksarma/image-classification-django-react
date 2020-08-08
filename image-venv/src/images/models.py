a1d6715790f1c99ea7e731dc24d3c3eb7b62fa2f
raajtilaksarma <raajtilaksarma@gmail.com>
2020-08-08 19:14:00 +0530

    add image model

--- image-venv/src/images/models.py
+++ image-venv/src/images/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    picture = models.ImageField()
+    classfield = models.CharField(max_length=200, blank=True)
+    uploaded = models.DateTimeField(auto_now_add=True)
